ec24cc2d86a3d937dbcc7cefbfa1ebffe82a372e
Luis Mario Ramirez Peralta <luismarioram99@gmail.com>
2021-01-07 00:22:06 -0600

    Navbar en progreso, logo creado

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,99 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-navigation-drawer absolute dark color="primary" v-model="drawer" left>
+      <v-list>
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-img
+              src="https://randomuser.me/api/portraits/women/85.jpg"
+            ></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
+
+        <v-list-item link class="mt-5">
+          <v-list-item-content>
+            <v-list-item-title class="title"> Invitado </v-list-item-title>
+            <v-list-item-subtitle
+              >Registrate para comprar telefonos.</v-list-item-subtitle
+            >
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list nav dense>
+        <v-list-item link to="/">
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-finance</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Estadisticas</v-list-item-title>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-plus-circle</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Nuevo anuncio</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="./assets/logo-phoneshop.png"
           transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
+          width="200"
         />
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-app-bar-nav-icon
+        class="hidden-sm-and-up"
+        @click="drawer = true"
+      ></v-app-bar-nav-icon>
+
+      <div class="hidden-xs-only">
+        <v-btn color="primary" elevation="0" to="/" active-class="lighten-1">
+          Inicio <v-icon class="mx-2">mdi-home</v-icon></v-btn
+        >
+        <v-btn color="primary" elevation="0"
+          >Estadisticas <v-icon class="mx-2">mdi-finance</v-icon></v-btn
+        >
+        <v-btn color="primary" elevation="0"
+          >Nuevo Anuncio <v-icon class="mx-2">mdi-plus-circle</v-icon>
+        </v-btn>
+      </div>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <HelloWorld />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     HelloWorld,
   },
 
   data: () => ({
-    //
+    drawer: false,
   }),
 };
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,16 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+        iconfont: 'mdi', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+    },
+    theme: {
+        themes: {
+            light: {
+                primary: '#240B36',
+                secondary: '#51D3D7',
+                accent: '#F0F66E'
+            }
+        }
+    }
 });
