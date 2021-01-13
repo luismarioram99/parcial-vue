1f07084748bf8b1926ac0bc20b7e45a0b61e6721
Luis Mario Ramirez Peralta <luismarioram99@gmail.com>
2021-01-13 00:10:27 -0600

    :Añadido primer chart'

--- src/views/Estadisticas.vue
+++ src/views/Estadisticas.vue
@@ -6,38 +6,58 @@
       <v-row>
         <v-col col="12" md="6">
           <v-card elevation="15">
-            <v-card-title>
-              Marcas publicadas
-            </v-card-title>
+            <v-card-title> Marcas publicadas </v-card-title>
             <v-divider></v-divider>
             <v-card-text>
-              
+              <LineChart :chart-data="datacollection" />
+              <button @click="fillData()">Randomize</button>
             </v-card-text>
           </v-card>
         </v-col>
         <v-col col="12" md="6">
           <v-card elevation="15">
-            <v-card-title>
-              Marcas Compradas
-            </v-card-title>
+            <v-card-title> Marcas Compradas </v-card-title>
             <v-divider></v-divider>
-            <v-card-text>
-
-            </v-card-text>
+            <v-card-text> </v-card-text>
           </v-card>
         </v-col>
       </v-row>
-
     </v-container>
   </div>
 </template>
 
 <script>
+import LineChart from "../charts/LineChart.js";
 export default {
-
-}
+  name: "Estadisticas",
+  data() {
+    return {
+      datacollection: null,
+    };
+  },
+  components: {
+    LineChart,
+  },
+  methods: {
+    fillData() {
+      this.datacollection = {
+        labels: [this.getRandomInt(), this.getRandomInt()],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#AA0000",
+            data: [this.getRandomInt(), this.getRandomInt(),this.getRandomInt(),this.getRandomInt()],
+          }
+          
+        ],
+      };
+    },
+    getRandomInt() {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
